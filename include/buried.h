55dc717e51a64a8d8fd8d0cdb3b6f499971426dc
zk_glb <1552535580@qq.com>
2024-03-17 22:36:02 +0800

    初次提交

---
+++ include/buried.h
@@ -0,0 +1,12 @@
+//
+// Created by 15525 on 2024/3/17.
+//
+
+#ifndef SDKINSTUDY_BURIED_H
+#define SDKINSTUDY_BURIED_H
+
+#endif//SDKINSTUDY_BURIED_H
+
+#pragma once
+
+int BuriedTest();
---
+++ src/buried.cpp
@@ -0,0 +1,11 @@
+//
+// Created by 15525 on 2024/3/17.
+//
+
+
+#include <iostream>
+
+int BuriedTest() {
+    std::cout << "Test";
+    return 1;
+}
